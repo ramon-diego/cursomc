9656b2c06fab88f9b24f6419509b2baa45efd707
Ramon Diego <ramon-diego@hotmail.com>
2018-06-30 21:05:35 -0300

    Produto e assiação muitos para muitos

--- src/main/java/com/ramonrocha/cursomc/CursomcApplication.java
+++ src/main/java/com/ramonrocha/cursomc/CursomcApplication.java
@@ -8,7 +8,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.ramonrocha.cursomc.domain.Categoria;
+import com.ramonrocha.cursomc.domain.Produto;
 import com.ramonrocha.cursomc.repositories.CategoriaRepository;
+import com.ramonrocha.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
 public class CursomcApplication implements CommandLineRunner{
@@ -16,15 +18,36 @@ public class CursomcApplication implements CommandLineRunner{
 	@Autowired
 	CategoriaRepository categoriaRepository;
 		
+	@Autowired
+	ProdutoRepository produtoRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
 	@Override
 	public void run(String... args) throws Exception {
+		
 		Categoria categoria1 = new Categoria(null, "Informática");
 		Categoria categoria2 = new Categoria(null, "Escritório");
 		
+		Produto produto1 = new Produto(null, "Computador", 2000.00);
+		Produto produto2 = new Produto(null, "Impressora", 800.00);
+		Produto produto3 = new Produto(null, "Mouse", 80.00);
+		
+		categoria1.getProdutos().addAll(Arrays.asList(produto1,produto2,produto3));
+		categoria1.getProdutos().addAll(Arrays.asList(produto2));
+		
+		produto1.getCategorias().addAll(Arrays.asList(categoria1));
+		produto2.getCategorias().addAll(Arrays.asList(categoria1, categoria2));
+		produto3.getCategorias().addAll(Arrays.asList(categoria1));
+		
+		
 		categoriaRepository.saveAll(Arrays.asList(categoria1, categoria2));
+		
+		produtoRepository.saveAll(Arrays.asList(produto1,produto2, produto3));
+		
+		
+		
 	}
 }

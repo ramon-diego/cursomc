fde03174f8484f40d923428ce827379b88f46e22
Ramon Diego <ramon-diego@hotmail.com>
2018-07-12 23:11:40 -0300

    Validação customizada de CPF e CNPJ na inserção de cliente

--- src/main/java/com/ramonrocha/cursomc/dto/ClienteNewDTO.java
+++ src/main/java/com/ramonrocha/cursomc/dto/ClienteNewDTO.java
@@ -2,28 +2,47 @@ package com.ramonrocha.cursomc.dto;
 
 import java.io.Serializable;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.validator.constraints.Length;
+
+import com.ramonrocha.cursomc.services.validation.ClienteInsert;
+
+@ClienteInsert
 public class ClienteNewDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
 
-
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Length(min=5, max=120, message="O tamanho deve ser entre 5 e 120 caracteres")
 	private String nome;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
+	@Email(message="E-mail inválido")
 	private String email;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String cpfOuCnpj;
+	
 	private Integer tipo;
-		
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String logradouro;
+	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String numero;
 	private String complemento;
 	private String bairro;
 	private String cep;
 	
-	
+	@NotEmpty(message="Preenchimento obrigatório")
 	private String telefone1;
+	
 	private String telefone2;
 	private String telefone3;
 	
-	private Integer cidade;
+	private Integer cidadeId;
 
 	public String getNome() {
 		return nome;
@@ -122,11 +141,12 @@ public class ClienteNewDTO implements Serializable{
 	}
 
 	public Integer getCidadeId() {
-		return cidade;
+		return cidadeId;
 	}
 
-	public void setCidadeId(Integer cidade) {
-		this.cidade = cidade;
+	public void setCidadeId(Integer cidadeId) {
+		this.cidadeId = cidadeId;
 	}
+	
 		
 }
---
+++ src/main/java/com/ramonrocha/cursomc/services/validation/ClienteInsertValidation.java
@@ -0,0 +1,41 @@
+package com.ramonrocha.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.ramonrocha.cursomc.domain.enums.TipoCliente;
+import com.ramonrocha.cursomc.dto.ClienteNewDTO;
+import com.ramonrocha.cursomc.resources.exception.FieldMessage;
+import com.ramonrocha.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidation implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Override
+	public void initialize(ClienteInsert ann) {
+		
+	}
+	
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		
+		List<FieldMessage> list = new ArrayList<>();
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCodigo()) && !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF Inválido"));
+		}
+		
+		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCodigo()) && !BR.isValidCNPJ(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
+		}
+		
+		
+		for(FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName()).addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+}
